515fa0e423f478c658e12e4515a7f20da677a774
akosdabasi <dabasi.akos@gmail.com>
2024-01-21 21:40:54 +0100

    ex 1.5: part1/courseinfo refactor

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -1,24 +1,27 @@
 const Header = ({course}) => <h1>{course}</h1> ;
 
-const Part = ({part, idx}) => <p key={idx}>{part.title} {part.exercises}</p>
+const Part = ({part, idx}) => <p key={idx}>{part.name} {part.exercises}</p>
 
 const Content = ({parts}) => parts.map((part, idx) => <Part part={part} key={idx} idx={idx}/>);
 
 const Total = ({parts}) => <p>Number of exercises {parts[0].exercises + parts[1].exercises + parts[2].exercises}</p>
 
 const App = () => {
-  const course = 'Half Stack application development'
-  const parts = [
-    {title: 'Fundamentals of React', exercises: 10},
-    {title: 'Using props to pass data', exercises: 7},
-    {title: 'State of a component', exercises: 14} 
-  ]
+  const course = {
+
+    name: 'Half Stack application development',
+    parts: [
+      {name: 'Fundamentals of React', exercises: 10},
+      {name: 'Using props to pass data', exercises: 7},
+      {name: 'State of a component', exercises: 14} 
+    ]
+  }
 
   return (
     <div>
-      <Header course = {course}/>
-      <Content parts = {parts}/>
-      <Total parts = {parts}/>
+      <Header course = {course.name}/>
+      <Content parts = {course.parts}/>
+      <Total parts = {course.parts}/>
     </div>
   )
 }
