7deb35fbfd37e8c25243a04a4134cf3e6840dcc4
akosdabasi <dabasi.akos@gmail.com>
2024-01-22 15:45:05 +0100

    ex 1.11 unicafe

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,17 +1,57 @@
 import { useState } from 'react'
 
+const StatisticLine = ({name, value}) => <p>{name} {value}</p>
+
 const Statistics = ({good, neutral, bad}) => {
   let all = good + neutral + bad;
   if(all){
     return (
-      <>
+     /* <>
         <h1>Statistics</h1>
-        <p>good {good}</p>
-        <p>neutral {neutral}</p>
-        <p>bad {bad}</p>
-        <p>all {all}</p>
-        <p>avarage {(good - bad)/all}</p>
-        <p>positive {good/all*100} %</p>
+        <StatisticLine name={'good'} value={good}/>
+        <StatisticLine name={'neutral'} value={neutral}/>
+        <StatisticLine name={'bad'} value={bad}/>
+        <StatisticLine name={'all'} value={all}/>
+        <StatisticLine name={'avarage'} value={(good - bad)/all}/>
+        <StatisticLine name={'positive'} value={good/all*100}/>
+      </>*/
+      <>
+      <br/>
+      <br/>
+      <table border="1">
+        <thead>
+          <tr>
+              <th>Statistic</th>
+              <th>Value</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+              <td>good</td>
+              <td>{good}</td>
+          </tr>
+          <tr>
+              <td>neutral</td>
+              <td>{neutral}</td>
+          </tr>
+          <tr>
+              <td>bad</td>
+              <td>{bad}</td>
+          </tr>
+          <tr>
+              <td>all</td>
+              <td>{all}</td>
+          </tr>
+          <tr>
+              <td>avarage</td>
+              <td>{(good - bad)/all}</td>
+          </tr>
+          <tr>
+              <td>positive</td>
+              <td>{good/all*100} %</td>
+          </tr>
+        </tbody>
+    </table>
       </>
     ) 
   }
