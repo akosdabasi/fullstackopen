d708ccf1f0f661f6ed7efcc7fadac2dfe5c39434
akosdabasi <dabasi.akos@gmail.com>
2024-01-23 18:40:34 +0100

    2.11 phonebook using fetch

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,19 +1,21 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import PhoneBook from './components/PhoneBook'
 import Search from './components/Search'
-import  Form  from './components/Form'
-
+import Form from './components/Form'
+import axios from 'axios'
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ])
+  const [persons, setPersons] = useState([])
   const [newName, setNewName] = useState('')
   const [newPhone, setNewPhone] = useState('')
   const [search, setSearch] = useState('')
+
+  useEffect(()=>{
+
+    axios.get('http://localhost:3001/persons')
+      .then(res => setPersons(res.data))
+
+  }, [])
 
   const handleNameChange = (e)=>{
     //console.log(e);
@@ -28,7 +30,7 @@ const App = () => {
   const handleNameSubmit = (e)=>{
     e.preventDefault();
     //console.log(e);
-    const person = {name: newName, phone: newPhone};
+    const person = {name: newName, number: newPhone};
     if(persons.find(obj => obj.name === person.name) === undefined)
       setPersons(persons.concat(person))
     else
