9b98c0ff383d705c0eeec8297f80a6a06e88f60f
akosdabasi <dabasi.akos@gmail.com>
2024-01-24 08:58:40 +0100

    2.17 phonebook: notifications

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -3,17 +3,22 @@ import PhoneBook from './components/PhoneBook'
 import Search from './components/Search'
 import Form from './components/Form'
 import axios from 'axios'
+import numbersService from './services/numbers'
+import PopUp from './components/PopUp'
 
 const App = () => {
   const [persons, setPersons] = useState([])
   const [newName, setNewName] = useState('')
   const [newPhone, setNewPhone] = useState('')
   const [search, setSearch] = useState('')
+  const [message, setMessage] = useState('')
+  const [messageType, setMessageType] = useState('')
 
   useEffect(()=>{
 
-    axios.get('http://localhost:3001/persons')
-      .then(res => setPersons(res.data))
+    numbersService.getAll()
+      .then(numbers => setPersons(numbers))
+    console.log('init phonebook');
 
   }, [])
 
@@ -30,11 +35,31 @@ const App = () => {
   const handleNameSubmit = (e)=>{
     e.preventDefault();
     //console.log(e);
-    const person = {name: newName, number: newPhone};
-    if(persons.find(obj => obj.name === person.name) === undefined)
-      setPersons(persons.concat(person))
-    else
-      alert(`${newName} is already in the phonebook`)
+    const foundPerson = persons.find(obj => obj.name === newName);
+
+    if(!foundPerson)
+    {
+      numbersService.create({name: newName, number: newPhone})
+        .then(newPerson => {
+          setPersons(persons.concat(newPerson));
+          setMessage(`${newPerson.name} has been added to the phonebook`);
+          setMessageType(`notification`); 
+          setTimeout(() => setMessage(null), 3000)
+        })
+    }
+    else if(window.confirm(`${newName} is already in the phonebook.\nDo you wish to update his number?`))
+    {
+      const newPerson = {...foundPerson, number: newPhone};
+      numbersService.update(newPerson)
+        .then( _ => {
+          setPersons(persons.map((person) => person.id === foundPerson.id ? newPerson : person));
+          setMessage(`${newPerson.name}'number has been updated`);
+          setMessageType(`notification`); 
+          setTimeout(() => setMessage(null), 3000)  
+        })
+    }
+      
+        
   }
 
   const handleSearchChange = (e)=>{
@@ -42,14 +67,33 @@ const App = () => {
     setSearch(e.target.value); 
   }
 
+  const handleDelete = (id) => {
+    if(window.confirm(`Are you sure you want to delete it?`))
+      numbersService.remove(id)
+        .then(res => {
+          setPersons(persons.filter(person => person.id !== id));
+          setMessage(`Person has been deleted from the phonebook`);
+          setMessageType(`notification`); 
+          setTimeout(() => setMessage(null), 3000)
+        })
+        .catch( res => {
+          console.log(res);
+          setMessage(`res`);
+          setMessageType(`error`); 
+          setTimeout(() => setMessage(null), 3000)
+        })
+  }
+
+
   return (
     <div>
       <h1>Phonebook</h1>
+      <PopUp message={message} className={messageType}/>
       <Search handleSearchChange={handleSearchChange}/>
       <h2>Add new</h2>
       <Form handleNameChange={handleNameChange} handlePhoneChange={handlePhoneChange} handleNameSubmit={handleNameSubmit}/>
       <h2>Numbers</h2>
-      <PhoneBook persons={persons} search={search}/>
+      <PhoneBook persons={persons} search={search} handleDelete={handleDelete}/>
       
     </div>
   )
--- part2/phonebook/src/components/PhoneBook.jsx
+++ part2/phonebook/src/components/PhoneBook.jsx
@@ -1,3 +1,7 @@
-const PhoneBook = ({persons, search}) => persons.filter(person => person.name.toLowerCase().startsWith(search.toLocaleLowerCase())).map(person => <p key={person.name}>{person.name} {person.number}</p>)
+const PhoneBook = ({persons, search, handleDelete}) => persons.filter(person => person.name.toLowerCase().startsWith(search.toLocaleLowerCase())).map(person => 
+<span key={person.id}>
+    <p>{person.name} {person.number}</p>
+    <button onClick={() => handleDelete(person.id)}>delete</button>
+</span>)
 
 export default PhoneBook
